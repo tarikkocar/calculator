a3e8430002da06c59a03eb79218de64774951f0d
ahmetkocar <ahmetkocar@gmail.com>
2022-12-18 18:56:24 +0300

    Created basic functions and UI

---
+++ calculator.js
@@ -0,0 +1,24 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function power(a, b) {
+    return a ** b;
+}
+
+function operate(a, b, f) {
+    return f(a, b);
+}
+
