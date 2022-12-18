717c6bf1599864d2618b14f9d1007ae4ccaf4162
ahmetkocar <ahmetkocar@gmail.com>
2022-12-18 23:14:50 +0300

    Added the functionality for basic mathematical operations

--- calculator.js
+++ calculator.js
@@ -22,3 +22,56 @@ function operate(a, b, f) {
     return f(a, b);
 }
 
+const operatorObject = {"add": add, "subtract": subtract, "multiply": multiply, "divide": divide, "power": power}
+let memory = {firstNumber: Infinity, secondNumber: Infinity, operator: ""}
+
+const display = document.querySelector(".display");
+const store = document.querySelector(".store");
+
+function changeDisplay(e) {
+    let newNumber = parseInt(e.target.innerHTML);
+    if (display.innerHTML.length < 18) {
+        display.innerHTML += newNumber;
+    }
+}
+
+const digits = document.querySelectorAll(".digit");
+for (i=0; i < digits.length; i++) {
+    digits[i].addEventListener("click", changeDisplay);
+}
+
+function changeStore(e) {
+    if (display.innerHTML.length > 0) {
+        store.innerHTML = `${display.innerHTML} ${e.target.innerHTML}`;
+        memory.firstNumber = parseInt(display.innerHTML);
+        memory.operator = e.target.id;
+        display.innerHTML = "";
+    }
+}
+
+const operators = document.querySelectorAll(".operator");
+for (i=0; i < operators.length; i++) {
+    operators[i].addEventListener("click", changeStore);
+}
+
+function calculate(e) {
+    if (display.innerHTML.length > 0 && store.innerHTML.length > 0) {
+        memory.secondNumber = parseInt(display.innerHTML);
+        store.innerHTML += ` ${display.innerHTML}`;
+        display.innerHTML = operate(memory.firstNumber, memory.secondNumber, operatorObject[memory.operator])
+    }
+}
+
+document.querySelector(".equal").addEventListener("click", calculate)
+
+document.querySelector(".clear").addEventListener("click", () => {
+    memory.firstNumber = Infinity;
+    memory.secondNumber = Infinity;
+    memory.operator = "";
+    store.innerHTML = "";
+    display.innerHTML = "";
+});
+
+document.querySelector(".delete").addEventListener("click", () => {
+    display.innerHTML = display.innerHTML.slice(0, -1);
+})
